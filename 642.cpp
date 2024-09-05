edef361345e81cb7df0bcc82d41d194d99299f88
mozammilali01 <mozammilali2018@gmail.com>
2024-09-05 08:22:28 +0530

    Add files via upload

---
+++ 642.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solution(){
+    int x,y; cin >>x >> y;
+    cout<<x/(y*2)<<endl;
+}
+
+int main() {
+	// your code goes here
+	int t; cin >> t;
+	while (t--)solution();
+	return 0;
+	    
+	   
+	
+
+}
+//                                                                 viod used solution 
