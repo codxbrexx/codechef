f895bb6239de9a78e884acf967d198085527acac
codxbrexx <mozammilali2018@gmail.com>
2024-09-08 21:22:03 +0530

    Add files via upload

---
+++ arry_716.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t; cin >> t; 
+    while (t--)
+    {
+        int n; cin >> n;
+        int arr[n];
+        int cnt =0;
+        for (int i =0; i < n; i++)
+        {
+            cin >> arr[i];
+            if (arr[i]>= 10 && arr[i] <= 60)
+            {
+                cnt++;
+            }
+        }cout << cnt << endl;
+    }return 0;
+    
+}
+	
